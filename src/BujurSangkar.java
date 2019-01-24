3c0e0facf2658a514aea75040a11a06091eaec41
Aditya <ziziaditya1101@gmail.com>
2019-01-24 09:56:09 +0700

    Aditya

---
+++ src/BujurSangkar.java
@@ -0,0 +1,13 @@
+public class BujurSangkar {
+    public static void main(String [] args){
+        BujurSangkar bj = new BujurSangkar();
+        bj.kel();
+    }
+    public int sisi = 10;
+    public double kel;
+
+    public void kel(){
+        kel = 4 * sisi;
+        System.out.println("Keliling bujur sangkar = " + kel);
+    }
+}
